9681d40381b955688a4b5eb2a49c03e2dc10c887
Naman0901 <73291949+Naman0901@users.noreply.github.com>
2023-02-01 09:56:10 +0530

    Update admin.js

--- Quiz Backend/routers/admin.js
+++ Quiz Backend/routers/admin.js
@@ -4,6 +4,7 @@ const adminControl = require('../controllers/adminControllers');
 router.use(express.json());
 
 router.post("/test",adminControl.addtest);
+router.delete("/test/:id",adminControl.deltest);
 router.post("/question",adminControl.addquestion);
 router.get("/question/:id",adminControl.getquestion);
 router.put("/question/:id",adminControl.updquestion);
